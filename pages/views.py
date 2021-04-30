6bf17bbd59c3da39b0322e7788bc1429b03fcfb2
Younes <younesmalek2014@gmail.com>
2021-04-30 13:43:22 +0200

    Home page fetch latest listings

--- pages/views.py
+++ pages/views.py
@@ -1,7 +1,13 @@
 from django.shortcuts import render
 
+from listings.models import Listing
+
 def index(request):
-    return render(request, 'pages/index.html')
+    latest_listings = Listing.objects.order_by('-list_date').filter(is_published=True)[:3]
+    context = {
+        'latest_listings': latest_listings
+    }
+    return render(request, 'pages/index.html', context)
 
 def about(request):
     return render(request, 'pages/about.html')
