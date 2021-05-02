9234fda5315f5d754693c4891c95b462a4854128
younesm@hpy <younesmalek2014@gmail.com>
2021-05-02 12:06:13 +0200

    About page fetch realtors data

--- pages/views.py
+++ pages/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render
 
 from listings.models import Listing
+from realtors.models import Realtor
 
 def index(request):
     latest_listings = Listing.objects.order_by('-list_date').filter(is_published=True)[:3]
@@ -10,4 +11,13 @@ def index(request):
     return render(request, 'pages/index.html', context)
 
 def about(request):
-    return render(request, 'pages/about.html')
+    # Get all realtors
+    realtors = Realtor.objects.order_by('-hire_date')
+    # Get mvp realtors
+    mvp_realtors = Realtor.objects.all().filter(is_mvp=True)
+
+    context = {
+        'realtors': realtors,
+        'mvp_realtors': mvp_realtors
+    }
+    return render(request, 'pages/about.html', context)
