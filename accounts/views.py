0959dabe4f6790c7f6274662e3886cb149b85941
Younes <younesmalek2014@gmail.com>
2021-05-06 10:43:27 +0200

    Display inquiries on user dashboard

--- accounts/views.py
+++ accounts/views.py
@@ -2,6 +2,8 @@ from django.shortcuts import render, redirect
 from django.contrib import messages
 from django.contrib.auth.models import User, auth
 
+from contacts.models import Contact
+
 def register(request):
     if request.method == 'POST':
         # Get form values
@@ -62,7 +64,12 @@ def login(request):
         return render(request, 'accounts/login.html')
 
 def dashboard(request):
-    return render(request, 'accounts/dashboard.html')
+    contacts = Contact.objects.order_by('-contact_date').filter(user_id=request.user.id)
+    context = {
+        'contacts': contacts,
+    }
+
+    return render(request, 'accounts/dashboard.html', context)
 
 def logout(request):
     if request.method == 'POST':
